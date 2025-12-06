35f8c3058bb6ee06b547b795e663c63d711382e2
Kev <15689588+Kevsnz@users.noreply.github.com>
2025-12-06 13:55:28 +0500

    Take api keys from environment on config loading

--- src/config.rs
+++ src/config.rs
@@ -18,5 +18,11 @@ pub(crate) struct Config {
 pub(crate) fn load_config() -> Config {
     let config_file = std::env::var("AR_CONFIG_FILE").unwrap_or("./config.json".to_string());
     let config_str = std::fs::read_to_string(config_file).unwrap();
-    serde_json::from_str(&config_str).unwrap()
+    let mut config: Config = serde_json::from_str(&config_str).unwrap();
+
+    for model_config in config.models.values_mut() {
+        model_config.api_key = std::env::var(model_config.api_key.clone()).unwrap_or_default();
+    }
+
+    config
 }
